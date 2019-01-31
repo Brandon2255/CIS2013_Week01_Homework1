25dae91c84a10b6070059d31b717d72cf5bc8c14
Brandon2255 <46945117+Brandon2255@users.noreply.github.com>
2019-01-30 18:29:09 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//CIS2013_Week01_Homework.cpp
+//Brandon Martin
+//brandon.martin10@okstate.edu
+
+#include <iostream>
+int main() {
+using namespace std;
+
+cout << "===============================================================\n";
+cout << endl;
+cout << "Brandon Martin\n";
+cout << "Network Engineer\n";
+cout << endl;
+cout << endl;
+cout << "1416 NW Somestreet" << "\t\tP: (405) 813-7432\n";
+cout << "Oklahoma City, OK" << "\t\tE: brandon.martin10@okstate.edu\n";
+cout << "\t\t\t\tG: github.com/Brandon2255\n";
+cout << endl;
+cout << "===============================================================\n";
+	return 0;
+}
